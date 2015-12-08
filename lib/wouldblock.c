d6c0eb16fb1f9b612929d636e450eca454cc81f9
Andrew Canaday <andrew.canaday@nytimes.com>
2015-12-08 11:07:13 -0500

    First version that compiles.

--- lib/wouldblock.c
+++ lib/wouldblock.c
@@ -9,14 +9,46 @@
  *
  *===========================================================================*/
 
-#include <stdarg.h>
-#include <stdio.h>
-#include "config.h"
-#include "wouldblock.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
 
-const char* wouldblock_version_str(void)
+#include <errno.h>
+#include <stdarg.h>
+
+#include "wb_config.h"
+
+const char*
+wouldblock_version_str(void)
 {
     return VERSION;
 };
+
+#if HAVE_RECV
+ssize_t
+recv(int socket, void *buffer, size_t length, int flags)
+{
+    return 0;
+};
+#endif /* HAVE_RECV */
+
+#if HAVE_SEND
+ssize_t
+send(int socket, const void *buffer, size_t length, int flags)
+{
+    return 0;
+};
+#endif /* HAVE_SEND */
+
+#if HAVE_ACCEPT
+int
+accept(
+    int socket,
+    struct sockaddr *restrict address,
+    socklen_t *restrict address_len)
+{
+    return 0;
+};
+#endif /* HAVE_ACCEPT */
 
 /* EOF */
