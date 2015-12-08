f1e6313ebdd0d39348a773edb4989444b4102280
Andrew Canaday <andrew.canaday@nytimes.com>
2015-12-08 10:00:04 -0500

    Initial commit.

---
+++ autogen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+autoreconf -vif
+
+# EOF
---
+++ include/wouldblock.h
@@ -0,0 +1,26 @@
+/*=============================================================================
+ * libwouldblock: C Library for Non-blocking IO Testing
+ *
+ * The Apache 2.0 License
+ * 
+ * Copyright (c) 2015 Andrew Canaday
+ * 
+ * <INSERT LICENSE>
+ *
+ *===========================================================================*/
+
+#ifndef WOULDBLOCK_WOULDBLOCK_H
+#define WOULDBLOCK_WOULDBLOCK_H
+
+/*==================*
+ *===-- Types: --===*
+ *==================*/
+
+/*======================*
+ *===-- Functions: --===*
+ *======================*/
+const char* wouldblock_version_str(void);
+
+#endif /* WOULDBLOCK_WOULDBLOCK_H */
+
+/* EOF */
